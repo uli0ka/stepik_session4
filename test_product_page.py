1258a4f2828e14a1c69b60e06da4ce73aac247ef
uli0ka <ptichka.yulya@yandex.ru>
2020-11-20 23:55:54 +0300

    lesson: content independence

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,12 @@
 from .pages.product_page import ProductPage
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', [0,1,2,3,4,5,6,
+                                  pytest.param(7, marks=pytest.mark.xfail),
+                                  8,9])
+
+def test_guest_can_add_product_to_basket(browser, link):
+	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
 	product_page = ProductPage(browser, link)
 	product_page.open()
 	product_page.add_product_to_basket()
