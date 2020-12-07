c68e75b26de24c9c38979c3862169325ad84b8b9
uli0ka <ptichka.yulya@yandex.ru>
2020-12-08 01:24:48 +0300

    release version

--- test_main_page.py
+++ test_main_page.py
@@ -11,17 +11,15 @@ class TestLoginFromMainPage():
 		page.open()
 		page.should_be_login_link()
 		page.go_to_login_page()
-
-		login_page = LoginPage(browser, browser.current_url)
-		login_page.should_be_login_page()
+		page = LoginPage(browser, browser.current_url)
+		page.should_be_login_page()
     
 	def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
 		link = "http://selenium1py.pythonanywhere.com/"
 		page = MainPage(browser, link)
 		page.open()
 		page.go_to_basket_page()
-
-		basket_page = BasketPage(browser, browser.current_url)
-		basket_page.should_be_empty_in_basket()
-		basket_page.should_be_text_about_empty_basket()
+		page = BasketPage(browser, browser.current_url)
+		page.should_be_empty_in_basket()
+		page.should_be_text_about_empty_basket()
 
--- test_product_page.py
+++ test_product_page.py
@@ -12,37 +12,37 @@ class TestUserAddToBasketFromProductPage():
 		page = LoginPage(browser, link)
 		page.open()
 		email = str(time.time()) + "@fakemail.org"
-		password = "pass99634"
+		password = str(time.time())
 		page.register_new_user(email, password)
 		page.should_be_authorized_user()
 
 	@pytest.mark.need_review
 	def test_user_can_add_product_to_basket(self, browser):
 		link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-		product_page = ProductPage(browser, link)
-		product_page.open()
-		product_page.add_product_to_basket()
-		product_page.solve_quiz_and_get_code()
-		product_page.should_be_name_product_matches_product_in_basket()
-		product_page.should_be_pirce_product_matches_price_basket()
+		page = ProductPage(browser, link)
+		page.open()
+		page.add_product_to_basket()
+		page.solve_quiz_and_get_code()
+		page.should_be_name_product_matches_product_in_basket()
+		page.should_be_pirce_product_matches_price_basket()
 
 	def test_user_cant_see_success_message(self, browser):
 		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-		product_page = ProductPage(browser, link)
-		product_page.open()
-		product_page.should_not_be_success_message()
+		page = ProductPage(browser, link)
+		page.open()
+		page.should_not_be_success_message()
 
 		
 class TestGuestFromProductPage():
 	@pytest.mark.need_review
 	def test_guest_can_add_product_to_basket(self, browser):
 		link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-		product_page = ProductPage(browser, link)
-		product_page.open()
-		product_page.add_product_to_basket()
-		product_page.solve_quiz_and_get_code()
-		product_page.should_be_name_product_matches_product_in_basket()
-		product_page.should_be_pirce_product_matches_price_basket()
+		page = ProductPage(browser, link)
+		page.open()
+		page.add_product_to_basket()
+		page.solve_quiz_and_get_code()
+		page.should_be_name_product_matches_product_in_basket()
+		page.should_be_pirce_product_matches_price_basket()
 
 	@pytest.mark.need_review
 	def test_guest_can_go_to_login_page_from_product_page(self, browser):
@@ -57,9 +57,9 @@ class TestGuestFromProductPage():
 		page = ProductPage(browser, link)
 		page.open()
 		page.go_to_basket_page()
-		basket_page = BasketPage(browser, browser.current_url)
-		basket_page.should_be_empty_in_basket()
-		basket_page.should_be_text_about_empty_basket()
+		page = BasketPage(browser, browser.current_url)
+		page.should_be_empty_in_basket()
+		page.should_be_text_about_empty_basket()
 
 	def test_guest_should_see_login_link_on_product_page(self, browser):
 		link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -70,18 +70,18 @@ class TestGuestFromProductPage():
 	@pytest.mark.xfail(reason = "task: negative check")
 	def test_guest_cant_see_success_message_after_adding_product_to_basket(self, browser):
 		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-		product_page = ProductPage(browser, link)
-		product_page.open()
-		product_page.add_product_to_basket()
-		product_page.should_not_be_success_message()
+		page = ProductPage(browser, link)
+		page.open()
+		page.add_product_to_basket()
+		page.should_not_be_success_message()
 
 	@pytest.mark.xfail(reason = "task: negative check")
 	def test_message_disappeared_after_adding_product_to_basket(self, browser):
 		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-		product_page = ProductPage(browser, link)
-		product_page.open()
-		product_page.add_product_to_basket()
-		product_page.should_not_be_success_message_wiht_disappeared()
+		page = ProductPage(browser, link)
+		page.open()
+		page.add_product_to_basket()
+		page.should_not_be_success_message_wiht_disappeared()
 
 
 
